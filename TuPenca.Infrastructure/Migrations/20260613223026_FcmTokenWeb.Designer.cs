b3ed96c202ec18d0de20368677904a2f7db1f3b5
Andres Varela <varelafervenza@gmail.com>
2026-06-13 23:02:24 -0300

    Se agrega migracion  FcmTokenWeb

--- TuPenca.Infrastructure/Migrations/20260613223026_FcmTokenWeb.Designer.cs
+++ TuPenca.Infrastructure/Migrations/20260613223026_FcmTokenWeb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace TuPenca.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260613223026_FcmTokenWeb")]
+    partial class FcmTokenWeb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TuPenca.Infrastructure/Migrations/20260613223026_FcmTokenWeb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TuPenca.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FcmTokenWeb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FcmTokenWeb",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FcmTokenWeb",
+                table: "Usuarios");
+        }
+    }
+}
